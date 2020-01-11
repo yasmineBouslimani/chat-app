b148c8dc6450c4ddc43027015712ca11e7c9156f
Yasmine <yasmine.bouslimani@gmail.com>
2020-01-11 14:30:51 +0100

    status done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import './avatar.css';
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-     <img className="avatar" src={props.image} alt="avatar"/>
-      <div className="status">
-        <div className="textuel">
-          <h4 className="name">{props.firstname} {props.lastname}</h4>
-        </div>
-        <div class="flex">
-          <div className={props.online? "status-online" : "status-offline"}> </div>
-          <p className="status-text">{props.online? "Online" : "Offline"}</p></div>
-      </div>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: this.props.online
+        };
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.image} alt="avatar"/>
+                    <div className="textuel">
+                        <h4 className="name">{this.props.firstname} {this.props.lastname}</h4>
+                    </div>
 
-    </div>
-  );
+                    <div className="status"
+                         onClick={event => {
+                             const newOnline = !this.state.online;
+                             this.setState({online: newOnline});
+                         }}>
+                        <div className="flex">
+                        <div className={this.state.online ? 'status-online' : 'status-offline'}></div>
+                        <p className="status-text">{this.props.online ? "Online" : "Offline"}</p>
+                    </div>
+                    </div>
+                </div>
+        );
+    }
 }
 export default Contact;
 
